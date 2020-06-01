52c449c88690e5666e429cf9bb09a4884faebcea
ShengXue97 <simshengxue1997@gmail.com>
2020-06-01 17:56:56 +0800

    ok

---
+++ build/precache-manifest.853c0b64405d386cb8c34dffc2453ee9.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c327640d3d2e4b65c3e6a1d0f6b34696",
+    "url": "/youzu-react/index.html"
+  },
+  {
+    "revision": "910639daa0461c49cbd1",
+    "url": "/youzu-react/static/css/2.62c87023.chunk.css"
+  },
+  {
+    "revision": "f0cb0490d35e86e701b0",
+    "url": "/youzu-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "910639daa0461c49cbd1",
+    "url": "/youzu-react/static/js/2.3a005440.chunk.js"
+  },
+  {
+    "revision": "5e24fe6fbd7796d01cc3c65271c594cb",
+    "url": "/youzu-react/static/js/2.3a005440.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0cb0490d35e86e701b0",
+    "url": "/youzu-react/static/js/main.23b9046e.chunk.js"
+  },
+  {
+    "revision": "05fa28a72e8f19ad5332",
+    "url": "/youzu-react/static/js/runtime-main.758ead5f.js"
+  },
+  {
+    "revision": "bc0371387c82522ebdfe510944e5a2ae",
+    "url": "/youzu-react/static/media/code.bc037138.png"
+  },
+  {
+    "revision": "f94615bc61e665490f6863f1cc2a7edc",
+    "url": "/youzu-react/static/media/drag.f94615bc.png"
+  },
+  {
+    "revision": "66ad084a42d17dd0c8cb5c2f9d4d65ee",
+    "url": "/youzu-react/static/media/lightbulb.66ad084a.png"
+  },
+  {
+    "revision": "347156928ec7f33d6b90b68acafb567d",
+    "url": "/youzu-react/static/media/page1.34715692.jpg"
+  },
+  {
+    "revision": "a7a37634a474afb8b839b1386d55c429",
+    "url": "/youzu-react/static/media/page10.a7a37634.jpg"
+  },
+  {
+    "revision": "b44dbe86d3af2e1589e43ca7e22c4d17",
+    "url": "/youzu-react/static/media/page11.b44dbe86.jpg"
+  },
+  {
+    "revision": "41c64cf8a60ebeb3cdc3a57d5d2f55bc",
+    "url": "/youzu-react/static/media/page12.41c64cf8.jpg"
+  },
+  {
+    "revision": "b09c4e5192afecb6f47a1e44c456d855",
+    "url": "/youzu-react/static/media/page13.b09c4e51.jpg"
+  },
+  {
+    "revision": "2e7541ff6232aa0a5f10d248f992f8ef",
+    "url": "/youzu-react/static/media/page14.2e7541ff.jpg"
+  },
+  {
+    "revision": "5a7ee4e2a10c35155c898a55461bd655",
+    "url": "/youzu-react/static/media/page15.5a7ee4e2.jpg"
+  },
+  {
+    "revision": "9c2d79eb4d379a47c70e60a75cdd7902",
+    "url": "/youzu-react/static/media/page16.9c2d79eb.jpg"
+  },
+  {
+    "revision": "1c4064612e3c8b1d86bce6a45b5a66cc",
+    "url": "/youzu-react/static/media/page17.1c406461.jpg"
+  },
+  {
+    "revision": "5b0646b0f1fe56e4a789ac666e94b0c1",
+    "url": "/youzu-react/static/media/page18.5b0646b0.jpg"
+  },
+  {
+    "revision": "643268340f4847a8c0a0c45464919d67",
+    "url": "/youzu-react/static/media/page19.64326834.jpg"
+  },
+  {
+    "revision": "a9445369728c09c579d94772523a69dc",
+    "url": "/youzu-react/static/media/page2.a9445369.jpg"
+  },
+  {
+    "revision": "9eb4d274ec0a969eddafda7b3df8fea3",
+    "url": "/youzu-react/static/media/page3.9eb4d274.jpg"
+  },
+  {
+    "revision": "b3a4f8f235b30e834fdc5da040184b1e",
+    "url": "/youzu-react/static/media/page4.b3a4f8f2.jpg"
+  },
+  {
+    "revision": "2870cdfc9baf1c38641ffdb430f012a1",
+    "url": "/youzu-react/static/media/page5.2870cdfc.jpg"
+  },
+  {
+    "revision": "8324c42e3a51cb6efbdddc8436cd4899",
+    "url": "/youzu-react/static/media/page6.8324c42e.jpg"
+  },
+  {
+    "revision": "af5608d90af2c40b1d51437084a87cc6",
+    "url": "/youzu-react/static/media/page7.af5608d9.jpg"
+  },
+  {
+    "revision": "904b4d29b2ca6b1f26a6d27fa2caf354",
+    "url": "/youzu-react/static/media/page8.904b4d29.jpg"
+  },
+  {
+    "revision": "c2c0b7802da2e086d6d78b933c2bba4f",
+    "url": "/youzu-react/static/media/page9.c2c0b780.jpg"
+  },
+  {
+    "revision": "f3b96ecf204dc6ecb22501b5b48cbdb4",
+    "url": "/youzu-react/static/media/user.f3b96ecf.png"
+  }
+]);
