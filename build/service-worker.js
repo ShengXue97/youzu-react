bc3e55f836bdce3a8fdc69dad8862e64e2040ac0
ShengXue97 <simshengxue1997@gmail.com>
2020-06-01 18:44:03 +0800

    fix pages

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/youzu-react/precache-manifest.853c0b64405d386cb8c34dffc2453ee9.js"
+  "/youzu-react/precache-manifest.295f67c8cc8d4576ebd9e83ab307d60f.js"
 );
 
 self.addEventListener('message', (event) => {
