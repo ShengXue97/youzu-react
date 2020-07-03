a690a04e7d5c018a2e9d103521e043ee7a1174c1
ShengXue97 <simshengxue1997@gmail.com>
2020-07-03 12:04:24 +0800

    add smart port

--- src/components/serverInfo.js
+++ src/components/serverInfo.js
@@ -1,3 +1,15 @@
-const serverInfo= "http://localhost,5000";
+import { ViewArraySharp } from "@material-ui/icons";
+import axios from 'axios'; 
 
+var serverInfo= "http://localhost,5000";
+const ip = serverInfo.split(",")[0]
+const port = serverInfo.split(",")[1]
+
+axios.post(ip + ":" + port + "/ping", {timeout : 1000 * 3})
+.then(response => {
+})
+.catch(error => {
+    var serverInfo= "http://ycampus.southeastasia.cloudapp.azure.com,5000";
+    serverInfo= "http://localhost,5000";
+})
 export default serverInfo;
